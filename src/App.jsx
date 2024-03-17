4e28b6074aab5e38c22d202d354aed4ec8d264cf
Zhu <57535849+DerekZZhu@users.noreply.github.com>
2024-03-16 21:38:06 -0700

    Added image changing function

--- src/App.jsx
+++ src/App.jsx
@@ -17,34 +17,26 @@ function App() {
   // console.log(convolve(img, IDENTITY, 5, 5));
 
   const canvasRef = useRef(null)
-  const [imageSize, setImageSize] = useState([0,0])
+  const images = [{name: "Vampire Deer", img_: vd}, {name: "lenna", img_:lenna}, {name:"This Man", img_:tm}]
 
   useEffect(() => {
+    drawImage(vd)
+  }, [])
+
+  const drawImage = (src) => {
     const img = new Image()
-    img.src = tm
+    img.src = src
     img.onload = () => {
       const canvas = canvasRef.current
       const ctx = canvas.getContext('2d')
       canvas.width = img.width
       canvas.height = img.height
-      setImageSize([img.naturalWidth, img.naturalHeight])
       ctx.drawImage(img, 0, 0)
     }
-  }, [])
+  }
 
-  function convolve(mat, filter) {
-    const newMatrix = zeros(mat._size[0]-2, mat._size[1]-2)
-    for (var i = 1; i < mat._size[0]-1; i++) {
-      for (var j = 1; j < mat._size[1]-1; j++) {
-        var sum = 0
-        var window = subset(mat, index(range(i-1, i+2), range(j-1, j+2)))
-
-        sum += dot(window._data[0], filter._data[0]) + dot(window._data[1], filter._data[1]) + dot(window._data[2], filter._data[2])
-        //console.log(sum);
-        newMatrix.set([i-1, j-1], sum)
-      }
-    }
-    return flatten(newMatrix)
+  const changeImage = (src) => {
+    drawImage(src)
   }
 
   function convolveAll(mat_r, mat_g, mat_b, filter) {
@@ -52,7 +44,6 @@ function App() {
     const holderArray = []
     for (var i = 1; i < mat_r._size[0]-1; i++) {
       for (var j = 1; j < mat_r._size[1]-1; j++) {
-        var sum = 0
         var window_r = subset(mat_r, index(range(i-1, i+2), range(j-1, j+2)))
         var window_g = subset(mat_g, index(range(i-1, i+2), range(j-1, j+2)))
         var window_b = subset(mat_b, index(range(i-1, i+2), range(j-1, j+2)))
@@ -60,7 +51,6 @@ function App() {
         var sum_r = dot(window_r._data[0], filter._data[0]) + dot(window_r._data[1], filter._data[1]) + dot(window_r._data[2], filter._data[2])
         var sum_g = dot(window_g._data[0], filter._data[0]) + dot(window_g._data[1], filter._data[1]) + dot(window_g._data[2], filter._data[2])
         var sum_b = dot(window_b._data[0], filter._data[0]) + dot(window_b._data[1], filter._data[1]) + dot(window_b._data[2], filter._data[2])
-        //console.log(sum);
 
         holderArray.push(sum_r)
         holderArray.push(sum_g)
@@ -103,29 +93,11 @@ function App() {
         redMatrix.set([y + padding, x + padding], data[i]);
         greenMatrix.set([y + padding, x + padding], data[i + 1]);
         blueMatrix.set([y + padding, x + padding], data[i + 2]);
-        alphaMatrix.push(data[i + 3])
       }
     }
     console.log(alphaMatrix);
-    // const nRedMatrix = convolve(redMatrix, IDENTITY)
-    // const nBlueMatrix = convolve(blueMatrix, IDENTITY)
-    // const nGreenMatrix = convolve(greenMatrix, IDENTITY)
-
-    // const combinedArray = new Uint8ClampedArray(width * height * 4);
-    // for (let y = 0; y < ((width * height) * 4); y+=4) {
-    //   //console.log(y);
-    //   combinedArray[y] = nRedMatrix.get([y/4])
-    //   combinedArray[y+1] = nGreenMatrix.get([y/4])
-    //   combinedArray[y+2] = nBlueMatrix.get([y/4])
-    //   combinedArray[y+3] = alphaMatrix[y/4]
-    // }
     
     const combinedArray = convolveAll(redMatrix, greenMatrix, blueMatrix, IDENTITY)
-    // console.log(
-    //   combinedArray
-    // );
-    // console.log(nRedMatrix, nGreenMatrix, nBlueMatrix);
-  
     const newImageData = new ImageData(combinedArray, width, height);
     ctx.putImageData(newImageData, 0, 0);
   }
@@ -133,9 +105,21 @@ function App() {
   return (
     <>
       <h1>UW CSE 455 Kernel Cooker</h1>
-      <canvas ref={canvasRef} />
+      <div>
+        {
+          images.map((img_, i) => {
+            console.log(img_);
+            return(
+              <button key={i} onClick={() => changeImage(img_.img_)}>{img_.name}</button>
+            )
+          })
+        }
+      </div>
+
+      <canvas ref={canvasRef} width={200}  height={200}/>
       <button onClick={modImage}>Apply Kernel</button>
       {/* <button onClick={refresh}>Refresh Image</button> */}
+
       <div>
         Built for CSE 455 by Derek Zhu and Ruslan Mukladheev
       </div>

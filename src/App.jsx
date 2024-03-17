19fea6b7002e011748c9aeff49e1e99f362a5399
Zhu <57535849+DerekZZhu@users.noreply.github.com>
2024-03-16 21:04:53 -0700

    Optimized convolve with convolveAll

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
 
   useEffect(() => {
     const img = new Image()
-    img.src = vd
+    img.src = tm
     img.onload = () => {
       const canvas = canvasRef.current
       const ctx = canvas.getContext('2d')
@@ -32,7 +32,7 @@ function App() {
     }
   }, [])
 
-  function convolve(mat, filter, width, height) {
+  function convolve(mat, filter) {
     const newMatrix = zeros(mat._size[0]-2, mat._size[1]-2)
     for (var i = 1; i < mat._size[0]-1; i++) {
       for (var j = 1; j < mat._size[1]-1; j++) {
@@ -47,8 +47,29 @@ function App() {
     return flatten(newMatrix)
   }
 
-  function convolveAll(mats, filter, width, height) {
+  function convolveAll(mat_r, mat_g, mat_b, filter) {
+    //const combinedArray = new Uint8ClampedArray(width * height * 4);
+    const holderArray = []
+    for (var i = 1; i < mat_r._size[0]-1; i++) {
+      for (var j = 1; j < mat_r._size[1]-1; j++) {
+        var sum = 0
+        var window_r = subset(mat_r, index(range(i-1, i+2), range(j-1, j+2)))
+        var window_g = subset(mat_g, index(range(i-1, i+2), range(j-1, j+2)))
+        var window_b = subset(mat_b, index(range(i-1, i+2), range(j-1, j+2)))
 
+        var sum_r = dot(window_r._data[0], filter._data[0]) + dot(window_r._data[1], filter._data[1]) + dot(window_r._data[2], filter._data[2])
+        var sum_g = dot(window_g._data[0], filter._data[0]) + dot(window_g._data[1], filter._data[1]) + dot(window_g._data[2], filter._data[2])
+        var sum_b = dot(window_b._data[0], filter._data[0]) + dot(window_b._data[1], filter._data[1]) + dot(window_b._data[2], filter._data[2])
+        //console.log(sum);
+
+        holderArray.push(sum_r)
+        holderArray.push(sum_g)
+        holderArray.push(sum_b)
+        holderArray.push(255)
+        // newMatrix.set([i-1, j-1], sum)
+      }
+    }
+    return new Uint8ClampedArray(holderArray)
   }
 
   const modImage = () => {
@@ -85,20 +106,21 @@ function App() {
         alphaMatrix.push(data[i + 3])
       }
     }
+    console.log(alphaMatrix);
+    // const nRedMatrix = convolve(redMatrix, IDENTITY)
+    // const nBlueMatrix = convolve(blueMatrix, IDENTITY)
+    // const nGreenMatrix = convolve(greenMatrix, IDENTITY)
 
-    const nRedMatrix = convolve(redMatrix, IDENTITY, newWidth, newHeight)
-    const nBlueMatrix = convolve(blueMatrix, IDENTITY, newWidth, newHeight)
-    const nGreenMatrix = convolve(greenMatrix, IDENTITY, newWidth, newHeight)
-
-    const combinedArray = new Uint8ClampedArray(width * height * 4);
-    for (let y = 0; y < ((width * height) * 4); y+=4) {
-      //console.log(y);
-      combinedArray[y] = nRedMatrix.get([y/4])
-      combinedArray[y+1] = nGreenMatrix.get([y/4])
-      combinedArray[y+2] = nBlueMatrix.get([y/4])
-      combinedArray[y+3] = alphaMatrix[y/4]
-    }
+    // const combinedArray = new Uint8ClampedArray(width * height * 4);
+    // for (let y = 0; y < ((width * height) * 4); y+=4) {
+    //   //console.log(y);
+    //   combinedArray[y] = nRedMatrix.get([y/4])
+    //   combinedArray[y+1] = nGreenMatrix.get([y/4])
+    //   combinedArray[y+2] = nBlueMatrix.get([y/4])
+    //   combinedArray[y+3] = alphaMatrix[y/4]
+    // }
     
+    const combinedArray = convolveAll(redMatrix, greenMatrix, blueMatrix, IDENTITY)
     // console.log(
     //   combinedArray
     // );
